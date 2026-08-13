b07f8ec5944b9b0b98f3838469a247f45c0e5df6
cubehan3 <cubehan3@gmail.com>
2026-08-13 16:40:39 +0800

    feat(opt): hoist loop-invariant instructions

--- include/passes/LoopInvHoist.hpp
+++ include/passes/LoopInvHoist.hpp
@@ -1,13 +1,7 @@
 #ifndef PASSES_LOOPINVHOIST_HPP
 #define PASSES_LOOPINVHOIST_HPP
 
-#include "Instruction.hpp"
-#include "LoopSearch.hpp"
-#include "Module.hpp"
 #include "PassManager.hpp"
-#include "Value.hpp"
-#include "llvm/ADT/DenseMap.h"
-#include "llvm/ADT/DenseSet.h"
 
 /// Hoists loop-invariant instructions out of loops (loop-invariant code
 /// motion, LICM).
@@ -16,13 +10,6 @@ public:
   LoopInvHoist(Module *m) : Pass(m) {}
 
   void run() override;
-
-private:
-  using LoopTree =
-      llvm::DenseMap<pass::BBset_t *, llvm::DenseSet<pass::BBset_t *>>;
-
-  void hoist_invariants(pass::BBset_t *loop, LoopTree &loop_tree,
-                        LoopInfo &loop_searcher, pass::BBset_t &vis);
 };
 
 #endif
